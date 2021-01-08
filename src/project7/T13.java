da65bc2f11fb6902e221973c52935505ca9e8a04
FPhoenixtree <309890974@qq.com>
2021-01-08 18:50:53 +0800

    提交

--- src/project7/T13.java
+++ src/project7/T13.java
@@ -1,10 +1,24 @@
 package project7;
-
+import java.util.*;
 public class T13 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	 public static void main(String[] args) {
+	     Scanner input = new Scanner(System.in);
+	     System.out.print("Enter ten integers: ");
+	     ArrayList<Integer> a = new ArrayList<Integer>();
+	     for(int i = 0;i < 10;++i){
+	         a.add(input.nextInt());
+	     }
+	     System.out.print("The distinct integers are ");
+	     removeDuplicate(a);
+	 }
+	 public static void removeDuplicate(ArrayList<Integer> b){
+	     Set<Integer> setlist = new HashSet<Integer>();
+	     for(int i = 0;i < 10;++i){
+	         int s = b.get(i);
+	         if(!setlist.contains(s)){
+	             System.out.print(s + " ");
+	             setlist.add(s);
+	         }
+	     }
+	 }
 }
